17e9878cff09808c3cf0a55e72fd9beb73a8be9a
Mohamed kamos <75431685+mohmedkamos@users.noreply.github.com>
2021-04-06 19:23:29 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+// function myFunction(){
+
+//     document.getElementById("dmo").innerHTML="Hello";
+// }
+
+// document.write(5+5);
+// //mesage box
+// window.alert(3+4); 
+
+//  console.log('4+4');
+// // var x,y,z;
+// x=4;
+// y=3;
+// z=x+y;
+//document.getElementById("dmo").innerHTML="result : "+z+".";
+
+
+// document.getElementById("dmo").innerHTML='mohmed kamos';
+ // document.getElementById("dmo").style.backgroundColor="red";
